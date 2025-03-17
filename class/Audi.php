74e90c41e8173a01d8ac2bf077827c7e53759a6b
Misaela <randrianasolo.misaela@gmail.com>
2025-03-17 10:23:00 +0300

    exemple plusieur trait, access attr class dans trait

--- class/Audi.php
+++ class/Audi.php
@@ -1,10 +1,20 @@
 <?php
  require_once "Car.php";
  require_once "MyTrait.php";
+ require_once "MyTrait2.php";
 
 class Audi extends Car {
-    use MyTrait;
+    use MyTrait, MyTrait2;
 
+    public $nombrePneud;
+
+    public function __construct($name)
+    {
+        parent::__construct($name);
+        $this->nombrePneud = 4;
+    
+    }
+    
     //Concretisation methode intro de la classe Abstraite Car
     public function intro() : string {
       return "Choose German quality! I'm an $this->name!";
--- index.php
+++ index.php
@@ -3,13 +3,15 @@
  require_once "./class/Audi.php";
 
 $myCar = new Audi("Audi");
-
+echo "<br>";
 echo $myCar->intro();
+echo "<br>";
 $myCar->setMatricule("0001 AAA");
-
 $myCar->myName = "Mietud";
-
+echo "<br>";
 echo $myCar->myName;
-
+echo "<br>";
 $myCar->sayHello();
+echo "<br>";
+$myCar->displayNbrPneud();
 
