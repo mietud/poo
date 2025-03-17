ac60709e1a0cf9626452da1728aa7a8f73fb19f9
Misaela <randrianasolo.misaela@gmail.com>
2025-03-17 11:50:48 +0300

    Exemple custom Exception

--- index.php
+++ index.php
@@ -1,9 +1,10 @@
 <?php
  require_once "./class/Car.php";
  require_once "./class/Audi.php";
+ require_once "./class/Etudiant.php";
+ require_once "./class/Exceptions/AgeException.php";
 
 $myCar = new Audi("Audi");
-echo "<br>";
 echo $myCar->intro();
 echo "<br>";
 $myCar->setMatricule("0001 AAA");
@@ -14,4 +15,20 @@ echo "<br>";
 $myCar->sayHello();
 echo "<br>";
 $myCar->displayNbrPneud();
+
+//Exception
+$etudiant = new Etudiant();
+echo "<br><strong>Custom Exception</strong>";
+echo "<br>";
+try{
+    $etudiant->setAge(-10);
+}
+catch(AgeException $e){
+    echo $e->getMessage();
+}
+echo "<br>";
+echo "Age : ".$etudiant->getAge();
+
+
+
 
